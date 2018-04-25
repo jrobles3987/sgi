caf6a25d573f21eddda5e66bb37796c64590e5aa
conetupa <38594614+conetupa@users.noreply.github.com>
2018-04-25 00:00:24 -0500

    cesar

--- application/views/configuracion/menumarcaequipos.php
+++ application/views/configuracion/menumarcaequipos.php
@@ -1,12 +1,14 @@
+<?php include("Vmodalnueva_marca.php");?>
+
 <div class="row container col-lg-12 col-center">
 	<div class="panel panel-default panel-fade">
 		<div class="panel-heading">
-			<span class="panel-title">				
+			<span class="panel-title">
 				<div class="pull-right">
-					<p><a  href="#" class="btn btn-primary btn-block" id="btn_fuente_Nuevas">Nuevas Marcas</a></p>                           
+					<p><a  href="#" class="btn btn-primary btn-block" id="btn_nueva_marca">Nuevas Marcas</a></p>
 				</div>
 				<div class="clearfix"></div>
-			</span>			
+			</span>
 		</div>
 		<div class="panel-body">
 			<div class="tab-content">
@@ -14,7 +16,20 @@
 					<div class="panel panel-default col-md-6 col-center" style="padding: 1px,1px,1px,1px">
 					    <center><B> Marcas Equipos</B><center/>
 					</div>
-					<div>
+<!-- /////inicio de tabla recargable////// -->
+<div id="tabla_marcas_ingresadas">
+<TABLE id="tablamarca" class="table table-striped table-bordered table-hover">
+<?php
+echo '<THEAD>';
+echo '<TR><TH>N°</TH><TH>Nombre</TH><TH>Estado</TH></TR>';
+echo '</THEAD>';
+echo '<TBODY>';
+echo '</TBODY>';
+?>
+</TABLE>
+</div>
+<!-- /////fin de dibujo de tabla//////			 -->
+					<!-- <div>
 						<TABLE id="tablaincidenciasfuentes" class="table table-striped table-bordered table-hover">
 							<?php
 								echo '<THEAD>';
@@ -25,72 +40,59 @@
 								if($listar_marcas){
 									foreach ($listar_marcas as $fila) {
 										$num++;
-										echo '<TR id="'.$fila->idmarca.'" onclick="myFunctionEstados(this)"><TD>'.$num.'</TD><TD>'.$fila->nombremarca.'</TD><TD>'.$fila->estado.'</TD></TR>'; 
+										echo '<TR id="'.$fila->idmarca.'" onclick="myFunctionEstados(this)"><TD>'.$num.'</TD><TD>'.$fila->nombremarca.'</TD><TD>'.$fila->estado.'</TD></TR>';
 									}
 								}
 								echo '</TBODY>';
 							?>
 						</TABLE>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</div>
 	</div>
 	</div>
-	<div class="modal-body">					
+	<div class="modal-body">
 	</div>
 
-<script>	
-	function myFunctionEstados(x)
-	{
-		$.ajax({
-            type: "POST",
-            url: "<?php echo base_url('incidencias/mostrarincidentes');?>",
-            data: {idincidencia: x.id},
-            success: function (data) {
-				var json1 = JSON.parse(data);
-				$('#txtidincidencia').val(x.id);
-				$('#txttituloincidencia').val(json1.tituloincidencia);
-				$('#fechaapertura').val(json1.fechaapertura);
-				$('#fechavencimiento').val(json1.fechavencimiento);
-				$('#selectestado').val(json1.idincidenciaestado);
-				$('#selecturgencia').val(json1.urgencia);
-				$('#selectimpacto').val(json1.impacto);
-				$('#selectprioridad').val(json1.prioridad);
-				$('#selectecnico').val(json1.tecnicoasignado);
-				$('#selecfuenteincidencia').val(json1.idincidenciafuente);
-				$('#seleclocalizacion').val(json1.idlugarincidente);
-				$('#txtareadescripcion').val(json1.descripcion);
-				$('#selectcategoria').val(json1.idcategoria);
-            },
-            error: function (xhr, exception) {
-				alert("error");
-            }
-	    });
-		$('#vmodalincidencia').modal({show:true});		
-	}
+<script>
+ReDibujaTablaCalificacion();
 
-	//inicia las tablas usando el plugin datatables
-	$('#tablaincidenciasfuentes').dataTable({
-		//quitar para paginacion por defecto
-		"lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "Todos"]]
-	});
+$('#tablamarca').dataTable({
+	//quitar para paginacion por defecto
+	"lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "Todos"]]
+});
 
-	function LimpiarModalIngresoIncidencias () {
-		$('#txttituloincidencia2').val('');
-		$('#selectestado2').val(1);
-		$('#selecturgencia2').val(1);
-		$('#selectimpacto2').val(1);
-		$('#selectprioridad2').val(1);
-		$('#selectecnico2').val(0);
-		$('#selectfuenteincidencia2').val(1);
-		$('#selectlocalizacion2').val(0);
-		$('#selectcategoria2').val(0);
-		$('#txtareadescripcion2').val('');
-	}
+function ReDibujaTablaCalificacion () {
+	$('#div_loading_cargando').css('display','inline');
+	$.ajax({
+					type: "POST",
+					url: "<?php echo base_url('marcasequipos/ReDibujarTablaMarca');?>",
+					data: {idincidencia: 1},
+					success: function (data) {
+			//var tabla = JSON.parse(data);
+			document.getElementById("tabla_marcas_ingresadas").innerHTML = "";
+			document.getElementById("tabla_marcas_ingresadas").innerHTML = data;
+			$('#tablamarca').dataTable({
+				//quitar para paginacion por defecto
+				"lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "Todos"]]
+			});
+					},
+					complete : function(xhr, status) {
+							$('#div_loading_cargando').css('display','none');
+					},
+					error: function (xhr, exception) {
+			alert("error");
+					}
+		});
+}
 
-	$('#btn_fuente_Nuevas').click(function() {
+////llamar modal
+	$('#btn_nueva_marca').click(function() {
 		LimpiarModalIngresoIncidencias();
-		$('#Vmodalincidencia_nueva').modal({show:true});
+		$('#Vmodalnueva_marca').modal({show:true});
 	});
+	function LimpiarModalIngresoIncidencias(){
+		$('#txt_nomarca').val("");
+	}
 </script>
