b616305721b9ac149ccb75ae512188d71516a19d
jrobles3987 <jrobles3987@users.noreply.github.com>
2018-04-24 23:58:02 -0500

    jonathan

--- application/views/equipos/menuingresosistemas.php
+++ application/views/equipos/menuingresosistemas.php
@@ -73,7 +73,7 @@
 	          	<div style="margin:5px"></div>
 	          	<div class="row">
                     <div class="col-md-8"></div>
-                    <div class="col-md-3">
+                    <div class="col-md-3" id="btn-guardar-sistema">
                         <p>
                             <a class="btn btn-primary btn-block">Guardar Datos</a>
                         </p>
@@ -128,45 +128,16 @@ $(document).ready(function() {
 		var retorno = true;
 		var numtipobien = document.getElementById("selecttipoequipo").length;
 		var numfamiliabien = document.getElementById("selectequipo").length;
-		var numsubfamiliabien = document.getElementById("selectsubequipo").length;
-		if ($('#preciocompra').val() == ''){
-			valorcompra = 0;
-		}else{
-			valorcompra = $('#preciocompra').val();
-		}
-
-		if ($('#vidautil').val() == ''){
-			vidautil = 0;
-			vidautiltiempo = '';
-		}else{
-			vidautil = $('#vidautil').val();
-			vidautiltiempo = ('#vidautiltiempo').val();
-		}
-
-		if ($('#garantia').val() == ''){
-			garantia = 0;
-			garantiatiempo = '';
-		}else{
-			garantia = $('#garantia').val();
-			garantiatiempo = ('#garantiatiempo').val();
-		}
+		var numsubfamiliabien = document.getElementById("selectsubequipo").length;		
 
 		dataform = {
 			idtipobien: $('#selecttipoequipo').val(),
 			idfamiliabien: $('#selectequipo').val(),
-			idsubfamiliabien: $('#selectsubequipo').val(),				
-			codequipo: $('#codequipo').val(),
-			codinventario: $('#codequipoinventario').val(),
-			fechacompra: $('#fechacompra').val(),
-			descripcion: $('#nombreequipo').val(),
-			valorcompra: valorcompra,
-			vidautil: vidautil,
-			garantia: $('#garantia').val(),
-			idpersonacustodio: $('#cmb_custodio').val(),
-			idlocalizacion: $('#cmb_localizacion').val(),
-			observacion: $('#descripcion').val(),
-			vidautiltiempo: $('#vidautiltiempo').val(),
-			garantiatiempo: $('#garantiatiempo').val()
+			idsubfamiliabien: $('#selectsubequipo').val(),
+			fechaingreso: $('#fechaingreso').val(),
+			idlocalizacion: $('#idlocalizacion').val(),
+			descripcion: $('#descripcion').val(),
+			codigosistema: $('#codigosistema').val()
 		}
 
 		retorno = Validar_Formularios2();			
@@ -195,14 +166,49 @@ $(document).ready(function() {
 				$("#selectequipo").css({'border':'1px solid red'});
 				msj_alerta();					
 			}
-		}			
-		
+		}		
 		return retorno;
 	}
 
-	
+	//GUARDAR EQUIPOS
+		$('#btn-guardar-sistema').click(function() {
+			if(validacion_formulario()){
+				$('#div_loading').css('display','inline');
+				$.ajax({
+		            type: "POST",
+		            url: "<?php echo base_url('equipo/GuardarEquiposComputador');?>",
+		            data: dataform,
+		            success: function (data) {
+		                var json = JSON.parse(data);
+		                $('#div_loading').css('display','none');
+		                if (json.res=="t") {
+		                	swal({
+							  title: "",
+							  text: "Datos Guardados\n Desea ingresar un nuevo equipo con las mismas características",
+							  type: "success",
+							  showCancelButton: true,
+							  confirmButtonText: "Si",
+							  cancelButtonText: "No",
+							  closeOnConfirm: true,
+							  closeOnCancel: true
+							},
+							function(isConfirm){
+							  if (!isConfirm) {
+							  	//location.reload(true);
+							  }
+							});
 
-	
+		                }else{
+		                	swal("","Ocurrio un error al guardar!","error");
+		                }
+		            },
+		            error: function (xhr, exception) {
+		            }
+		        });
+			}
+		});
+
+
 	/////////////////// busqueda de localizacion
 	var busqueda = {
 		ajax          : {
