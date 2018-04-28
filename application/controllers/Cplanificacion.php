bbf3ace7d3da17b555e8ba107d246a4fc9f919bc
jrobles3987 <jrobles3987@users.noreply.github.com>
2018-04-28 12:53:17 -0500

    Jonathan R

--- application/controllers/Cplanificacion.php
+++ application/controllers/Cplanificacion.php
@@ -2,7 +2,7 @@
 /**
  *
  */
-class Cplanificacioes extends CI_Controller
+class Cplanificacion extends CI_Controller
 {
   public function ReDibujarTablaPlanificaciones()
   {
@@ -39,58 +39,49 @@ class Cplanificacioes extends CI_Controller
 			show_404();
 		}
 	}
- public function guardar_plani_tec()
-{
-  if ($this->session->userdata('login'==TRUE)) {
-    $idplanificacion->$this->input->post('idplanificacion');
-    $idtecnico->$this->input->post('idtecnico');
-    $comentario->$this->input->post('comentario');
-    $this->load->model('planificacion');
-    $res_pla_tec =$this->planificacion->setguardarplanitec($idplanificacion,$idtecnico,$comentario);
-    $data= array(
-      "res" => $res_pla_tec->setguardarplanitec());
-    echo json_encode($data);
-  }else {
-    show_404();
-  }
-}
-
-public function GuardarPlanificaciones()
-{
-  if ($this->input->is_ajax_request()) {
-    $data = array(
-      'txttituloplanificacion'=>$this->input->post('txttituloplanificacion'),
-      'fechainicio-fechafin'		=> $this->input->post('fechainicio-fechafin'),
-      '#ui-datepicker-div'	=> $this->input->post('#ui-datepicker-div'),
-      'txtareadescripcion2'		=> $this->input->post('txtareadescripcion2'),
-      'txttituloincidencia2'	=> $this->input->post('txttituloincidencia2'),
-      'fechainicio-fechafin'		=> $this->input->post('fechainicio-fechafin'),
-      '#ui-datepicker-div'	=> $this->input->post('#ui-datepicker-div')
-    );
-
-    if ($data) {
-      $this->load->model('incidencia');
-      $modelos = $this->planificacion->setguardarplanificacion($data);
-
+  public function guardar_plani_tec()
+  {
+    if ($this->session->userdata('login'==TRUE)) {
+      $idplanificacion->$this->input->post('idplanificacion');
+      $idtecnico->$this->input->post('idtecnico');
+      $comentario->$this->input->post('comentario');
+      $this->load->model('planificacion');
+      $res_pla_tec =$this->planificacion->setguardarplanitec($idplanificacion,$idtecnico,$comentario);
       $data= array(
-        "res" => $modelos->f_ingreso_planificacion
+        "res" => $res_pla_tec->setguardarplanitec());
+      echo json_encode($data);
+    }else {
+      show_404();
+    }
+  }
+
+  public function GuardarPlanificaciones()
+  {
+    if ($this->input->is_ajax_request()) {
+      $data = array(
+        'tituloincidencia'=>$this->input->post('tituloincidencia'),
+        'fechainicio' => $this->input->post('fechainicio'),
+        'fechavencimiento'	=> $this->input->post('fechavencimiento'),
+        'tecnicosasignados'	=> $this->input->post('tecnicosasignados'),
+        'descripcion'	=> $this->input->post('descripcion'),
+        'localizacion'	=> $this->input->post('localizacion')
       );
 
-      //echo json_encode($data);
+      if ($data) {
+        $this->load->model('planificacion');
+        $modelos = $this->planificacion->setguardarplanificacion($data);
+
+        $data= array(
+          "res" => $modelos->f_ingreso_planificacion_tecnicos
+        );
+      }else{
+      }
+      echo json_encode($data);
     }else{
-      //show_404();
+      show_404();
     }
-    echo json_encode($data);
-  }else{
-    //show_404();
   }
-}
-
-
 
 }
 
-
-
-
- ?>
+?>
--- application/models/planificacion.php
+++ application/models/planificacion.php
@@ -7,48 +7,57 @@ class planificacion extends CI_Model
 
   function setguardarplanitec($idplanificacion='',$idtecnico='',$comentario='')
   {
-$result=$this->bd->query("INSERT INTO incidencias.planificacion_tecnicos(
-	idplanificacion, idtecnico, comentario)
-	VALUES ('".$idplanificacion."','".idtecnico."','".$comentario."');");
-  if ($result->num_rows()>0) {
-    return $result->row();
+    $result=$this->bd->query("INSERT INTO incidencias.planificacion_tecnicos(
+                          	   idplanificacion, idtecnico, comentario)
+                          	   VALUES ('".$idplanificacion."','".idtecnico."','".$comentario."');
+                            ");
+    if ($result->num_rows()>0) {
+      return $result->row();
     }else {
-    return null;
+      return null;
     }
-
   }
 
   public function getlistarplanificaiones()
-{
-    $result=$this->db->get('incidencias.v_listar_planificaciones');
-    if($result->num_rows()>0)
-    {
-      return $result->result();
-  }else{
-      return null;
-    }
-}
-
-public function setguardarplanificacion (){
-  $result = $this->db->query("SELECT incidencias.f_ingreso_planificacion(
-                              '".$data['txttituloplanificacion']."',
-                              '".$data['fechainicio-fechafin']."',
-                              '".$data['#ui-datepicker-div']."',
-                              '".$data['txtareadescripcion2']."',
-                              '".$data['idlocalizacion']."',
-                              null,
-                              '".$data['fechainicio-fechafin']."',
-                              '".$data['#ui-datepicker-div']."');");
-  if ($result->num_rows() > 0) {
-    $idplanifi = $result->row();
-
-
-    return $result->row();
-  }else {
-    return null;
+  {
+      $result=$this->db->get('incidencias.v_listar_planificaciones');
+      if($result->num_rows()>0)
+      {
+        return $result->result();
+    }else{
+        return null;
+      }
   }
 
-}
+  public function setguardarplanificacion ($data){
+    $bandera = true;
+    $result = $this->db->query("SELECT incidencias.f_ingreso_planificacion(
+                                '".$data['tituloincidencia']."',
+                                '".$data['fechainicio']."',
+                                '".$data['fechavencimiento']."',
+                                '".$data['descripcion']."',
+                                ".$data['localizacion']."
+                              );");
+    if ($result->num_rows() > 0) {
+      $idplanifi = $result->row();
+      for ($i=0; $i < count($data['tecnicosasignados']); $i++) {
+        if ($bandera) {
+          $result = $this->db->query("SELECT incidencias.f_ingreso_planificacion_tecnicos(
+                                      ".$idplanifi->f_ingreso_planificacion.",
+                                      ".$data['tecnicosasignados'][$i]."
+                                    );");
+          if ($result->num_rows() > 0) {
+            $bandera = $result->row();
+          }else {
+            $bandera = null;
+          }
+        }
+      }
+      return $bandera;
+    }else {
+      return null;
+    }
+  }
 
 
  public function setultimaplanificacion (){
@@ -62,10 +71,5 @@ public function setguardarplanificacion (){
 
  }
 
-
 }
-
-
-
-
- ?>
+?>
